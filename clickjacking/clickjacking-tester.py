449ffe16854b5662e5ea4ed168d48b570a51215f
Michele Romano <33063403+Mik317@users.noreply.github.com>
2018-10-16 14:40:31 +0200

    Update clickjacking-tester.py

--- clickjacking/clickjacking-tester.py
+++ clickjacking/clickjacking-tester.py
@@ -2,10 +2,12 @@ import os
 import webbrowser as bro
 
 url = input("Insert the url to test: ")
+el = input("ID of the element to point: ")
 html = '''<html><body>
-<iframe src="{}">
-</body></html>'''.format(url)
+<iframe src="{}"></iframe>
+</body></html>'''.format(url+'#'+el)
 
 with open("test.html", "w") as file:
     file.write(html)
     bro.open(os.path.abspath(file.name))
+    print(os.path.abspath(file.name))
